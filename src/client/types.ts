c338e016dad72ab0a6925bbb17969567fa117b71
alexlee-dev <alex@alexlee.dev>
2020-07-12 14:37:19 -0700

    📝 List Jobs - Gets list of jobs and displays jobs length

--- src/client/types.ts
+++ src/client/types.ts
@@ -1,9 +1,15 @@
-export interface Beer {
-  __v: string;
-  _id: string;
-  abv: number;
-  brewer: string;
+export interface Job {
+  company: string;
+  company_logo: string;
+  company_url: string;
+  created_at: string;
   description: string;
-  name: string;
-  type: string;
+  how_to_apply: string;
+  id: string;
+  location: string;
+  title: string;
+  type: JobType;
+  url: string;
 }
+
+export type JobType = "Full Time";
