b42b201e689ab4b0312d0136410ba9ed7844fb85
alexlee-dev <alex@alexlee.dev>
2020-07-13 10:44:33 -0700

    📝 Job Page - Render markdown

--- src/client/pages/Details.tsx
+++ src/client/pages/Details.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 import { useParams } from "react-router-dom";
+import ReactMarkdown from "react-markdown";
+
 import { getJobDetails } from "../api/github";
 
 const Details: React.SFC<{}> = () => {
@@ -18,17 +20,17 @@ const Details: React.SFC<{}> = () => {
       <h2>DETAILS</h2>
       {data && (
         <div>
-          <span>Company: ${data.company}</span>
-          <span>Company Logo: ${data.company_logo}</span>
-          <span>Company URL: ${data.company_url}</span>
-          <span>Created At: ${data.created_at}</span>
-          <span>How To Apply: ${data.how_to_apply}</span>
-          <span>ID: ${data.id}</span>
-          <span>Description: ${data.description}</span>
-          <span>Location: ${data.location}</span>
-          <span>Title: ${data.title}</span>
-          <span>Type: ${data.type}</span>
-          <span>URL: ${data.url}</span>
+          <span>Company: {data.company}</span>
+          <span>Company Logo: {data.company_logo}</span>
+          <span>Company URL: {data.company_url}</span>
+          <span>Created At: {data.created_at}</span>
+          <ReactMarkdown source={data.how_to_apply} />
+          <span>ID: {data.id}</span>
+          <ReactMarkdown source={data.description} />
+          <span>Location: {data.location}</span>
+          <span>Title: {data.title}</span>
+          <span>Type: {data.type}</span>
+          <span>URL: {data.url}</span>
         </div>
       )}
     </div>
