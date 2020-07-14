219b52094678f13beb325773dd3cb44d8d3df05f
alexlee-dev <alex@alexlee.dev>
2020-07-14 10:53:51 -0700

    🧹 OptionsPanel - Display unstyled information

--- src/client/components/OptionsPanel.tsx
+++ src/client/components/OptionsPanel.tsx
@@ -3,7 +3,62 @@ import * as React from "react";
 export interface OptionsPanelProps {}
 
 const OptionsPanel: React.SFC<OptionsPanelProps> = () => {
-  return <div>OPTIONSPANEL</div>;
+  return (
+    <div>
+      <label htmlFor="full-time">Full Time</label>
+      <input
+        id="full-time"
+        name="full-time"
+        // onChange={(e) => setFullTime(e.target.checked)}
+        type="checkbox"
+        value="full-time"
+      />
+      <label htmlFor="location-1">Chicago</label>
+      <input
+        id="location-1"
+        name="location1"
+        // onChange={(e) => handleCheckBox(e)}
+        type="checkbox"
+        value="Chicago"
+      />
+      <label htmlFor="location-2">Los Angeles</label>
+      <input
+        id="location-2"
+        name="location2"
+        // onChange={(e) => handleCheckBox(e)}
+        type="checkbox"
+        value="Los Angeles"
+      />
+      <label htmlFor="location-3">New York City</label>
+      <input
+        id="location-3"
+        name="location3"
+        // onChange={(e) => handleCheckBox(e)}
+        type="checkbox"
+        value="New York City"
+      />
+      <label htmlFor="location-4">San Francisco</label>
+      <input
+        id="location-4"
+        name="location4"
+        // onChange={(e) => handleCheckBox(e)}
+        type="checkbox"
+        value="San Francisco"
+      />
+      <label htmlFor="location-search">Location Search</label>
+      <input
+        id="location-search"
+        // onChange={(e) => setLocationSearch(e.target.value)}
+        type="text"
+        // value={locationSearch}
+      />
+      <button
+      // onClick={() => handleLocationSearch()}
+      >
+        Search
+      </button>
+    </div>
+  );
 };
 
 export default OptionsPanel;
--- src/client/pages/Search.tsx
+++ src/client/pages/Search.tsx
@@ -66,56 +66,6 @@ const Search: React.SFC<SearchProps> = () => {
         setJobs={setJobs}
       />
       <OptionsPanel />
-      <p>Jobs: {jobs.length}</p>
-      <p>Default City: Los Angeles</p>
-      <label htmlFor="full-time">Full Time</label>
-      <input
-        id="full-time"
-        name="full-time"
-        onChange={(e) => setFullTime(e.target.checked)}
-        type="checkbox"
-        value="full-time"
-      />
-      <label htmlFor="location-1">Chicago</label>
-      <input
-        id="location-1"
-        name="location1"
-        onChange={(e) => handleCheckBox(e)}
-        type="checkbox"
-        value="Chicago"
-      />
-      <label htmlFor="location-2">Los Angeles</label>
-      <input
-        id="location-2"
-        name="location2"
-        onChange={(e) => handleCheckBox(e)}
-        type="checkbox"
-        value="Los Angeles"
-      />
-      <label htmlFor="location-3">New York City</label>
-      <input
-        id="location-3"
-        name="location3"
-        onChange={(e) => handleCheckBox(e)}
-        type="checkbox"
-        value="New York City"
-      />
-      <label htmlFor="location-4">San Francisco</label>
-      <input
-        id="location-4"
-        name="location4"
-        onChange={(e) => handleCheckBox(e)}
-        type="checkbox"
-        value="San Francisco"
-      />
-      <label htmlFor="location-search">Location Search</label>
-      <input
-        id="location-search"
-        onChange={(e) => setLocationSearch(e.target.value)}
-        type="text"
-        value={locationSearch}
-      />
-      <button onClick={() => handleLocationSearch()}>Search</button>
       {jobs && jobs.map((job: Job) => <JobCard job={job} key={job.id} />)}
     </>
   );
