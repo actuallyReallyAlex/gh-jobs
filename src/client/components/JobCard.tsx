00d2c8955a6d640252f6df50df773d39b42bd29b
alexlee-dev <alex@alexlee.dev>
2020-07-14 08:55:48 -0700

    📃 JobCard - Display unstyled information

--- src/client/components/JobCard.tsx
+++ src/client/components/JobCard.tsx
@@ -7,7 +7,20 @@ export interface JobCardProps {
 
 const JobCard: React.SFC<JobCardProps> = (props: JobCardProps) => {
   const { job } = props;
-  return <div>JOBCARD</div>;
+  return (
+    <div>
+      <img alt="Company Logo" src={job.company_logo} />
+      <div>
+        <h4>{job.company}</h4>
+        <h3>{job.title}</h3>
+        {job.type === "Full Time" && <span>Full Time</span>}
+      </div>
+      <div>
+        <p>{job.location}</p>
+        <p>{job.created_at}</p>
+      </div>
+    </div>
+  );
 };
 
 export default JobCard;
