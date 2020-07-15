afdb78f765433965b91a9c20735d9cf409a97809
alexlee-dev <alex@alexlee.dev>
2020-07-14 22:20:06 -0700

    ⚛️ Redux - Incorporate 'saveState()' into 'store.subscribe()'

--- src/client/redux/store.ts
+++ src/client/redux/store.ts
@@ -1,11 +1,23 @@
 import { applyMiddleware, compose, createStore } from "redux";
 import thunk from "redux-thunk";
+import throttle from "lodash/throttle";
+import { saveState } from "../util";
 import rootReducer from "./reducers";
 
 const composeEnhancers =
   (window["__REDUX_DEVTOOLS_EXTENSION_COMPOSE__"] as typeof compose) || compose;
 
-export default createStore(
+const store = createStore(
   rootReducer,
   composeEnhancers(applyMiddleware(thunk))
 );
+
+store.subscribe(
+  throttle(() => {
+    saveState({
+      application: store.getState().application,
+    });
+  }, 1000)
+);
+
+export default store;
