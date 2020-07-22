c748b8554189c1d50cefd5893502952640678954
alexlee-dev <alex@alexlee.dev>
2020-07-22 09:38:14 -0700

    🦸‍♂️ Profile - Refactor Redundant Reducers

--- src/client/redux/reducers/application.ts
+++ src/client/redux/reducers/application.ts
@@ -32,42 +32,27 @@ const reducer = (
   state = initialState,
   action: ApplicationAction
 ): ApplicationState => {
+  let key: string;
+  let value;
+
+  if (action && action.payload) {
+    key = Object.keys(action.payload)[0];
+    value = action.payload[key];
+  }
+
   switch (action.type) {
-    case SET_CURRENT_JOBS: {
-      return { ...state, currentJobs: action.payload.currentJobs };
-    }
-    case SET_CURRENT_PAGE: {
-      return { ...state, currentPage: action.payload.currentPage };
-    }
-    case SET_FULL_TIME: {
-      return { ...state, fullTime: action.payload.fullTime };
-    }
-    case SET_IS_LOADING: {
-      return { ...state, isLoading: action.payload.isLoading };
-    }
-    case SET_JOBS: {
-      return { ...state, jobs: action.payload.jobs };
-    }
-    case SET_JOBS_FETCHED_AT: {
-      return { ...state, jobsFetchedAt: action.payload.jobsFetchedAt };
-    }
-    case SET_LOCATION_SEARCH: {
-      return { ...state, locationSearch: action.payload.locationSearch };
-    }
-    case SET_NOTIFICATION_MESSAGE: {
-      return {
-        ...state,
-        notificationMessage: action.payload.notificationMessage,
-      };
-    }
-    case SET_NOTIFICATION_TYPE: {
-      return { ...state, notificationType: action.payload.notificationType };
-    }
-    case SET_SEARCH_VALUE: {
-      return { ...state, searchValue: action.payload.searchValue };
-    }
+    case SET_CURRENT_JOBS:
+    case SET_CURRENT_PAGE:
+    case SET_FULL_TIME:
+    case SET_IS_LOADING:
+    case SET_JOBS:
+    case SET_JOBS_FETCHED_AT:
+    case SET_LOCATION_SEARCH:
+    case SET_NOTIFICATION_MESSAGE:
+    case SET_NOTIFICATION_TYPE:
+    case SET_SEARCH_VALUE:
     case SET_TOTAL_PAGES: {
-      return { ...state, totalPages: action.payload.totalPages };
+      return { ...state, [key]: value };
     }
     default:
       return state;
--- src/client/redux/reducers/user.ts
+++ src/client/redux/reducers/user.ts
@@ -30,53 +30,29 @@ export const initialState: UserState = {
   resetNewPassword: "",
 };
 
-// TODO - Refactor this. It's doing the same thing each time.
 const reducer = (state = initialState, action: UserAction): UserState => {
+  let key: string;
+  let value;
+
+  if (action && action.payload) {
+    key = Object.keys(action.payload)[0];
+    value = action.payload[key];
+  }
+
   switch (action.type) {
-    case SET_CONFIRM_PASSWORD: {
-      return { ...state, confirmPassword: action.payload.confirmPassword };
-    }
-    case SET_EDIT_EMAIL: {
-      return { ...state, editEmail: action.payload.editEmail };
-    }
-    case SET_EDIT_NAME: {
-      return { ...state, editName: action.payload.editName };
-    }
-    case SET_EMAIL: {
-      return { ...state, email: action.payload.email };
-    }
-    case SET_IS_EDITING_PROFILE: {
-      return { ...state, isEditingProfile: action.payload.isEditingProfile };
-    }
-    case SET_IS_LOGGED_IN: {
-      return { ...state, isLoggedIn: action.payload.isLoggedIn };
-    }
-    case SET_IS_RESETTING_PASSWORD: {
-      return {
-        ...state,
-        isResettingPassword: action.payload.isResettingPassword,
-      };
-    }
-    case SET_NAME: {
-      return { ...state, name: action.payload.name };
-    }
-    case SET_PASSWORD: {
-      return { ...state, password: action.payload.password };
-    }
-    case SET_RESET_CONFIRM_NEW_PASSWORD: {
-      return {
-        ...state,
-        resetConfirmNewPassword: action.payload.resetConfirmNewPassword,
-      };
-    }
-    case SET_RESET_CURRENT_PASSWORD: {
-      return {
-        ...state,
-        resetCurrentPassword: action.payload.resetCurrentPassword,
-      };
-    }
+    case SET_CONFIRM_PASSWORD:
+    case SET_EDIT_EMAIL:
+    case SET_EDIT_NAME:
+    case SET_EMAIL:
+    case SET_IS_EDITING_PROFILE:
+    case SET_IS_LOGGED_IN:
+    case SET_IS_RESETTING_PASSWORD:
+    case SET_NAME:
+    case SET_PASSWORD:
+    case SET_RESET_CONFIRM_NEW_PASSWORD:
+    case SET_RESET_CURRENT_PASSWORD:
     case SET_RESET_NEW_PASSWORD: {
-      return { ...state, resetNewPassword: action.payload.resetNewPassword };
+      return { ...state, [key]: value };
     }
     default:
       return state;
