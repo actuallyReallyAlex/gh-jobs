6539a8a385ddb8427bcd0ee9d5e465e555a39295
alexlee-dev <alex@alexlee.dev>
2020-07-24 10:04:07 -0700

    💅 Styled Components - Convert 'Header' CSS

--- src/client/components/Header.tsx
+++ src/client/components/Header.tsx
@@ -1,13 +1,29 @@
 import * as React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const StyledHeader = styled.header`
+  color: #282538;
+  font-family: Poppins;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 200;
+  line-height: 36px;
+  margin-bottom: 25px;
+  margin-top: 25px;
+
+  span {
+    font-weight: bold;
+  }
+`;
 
 // eslint-disable-next-line
 const Header: React.SFC<{}> = () => {
   return (
     <Link id="header-link" to="/">
-      <header>
-        <span className="bold">GitHub</span> Jobs
-      </header>
+      <StyledHeader>
+        <span>GitHub</span> Jobs
+      </StyledHeader>
     </Link>
   );
 };
