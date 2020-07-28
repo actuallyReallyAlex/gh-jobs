7c539847976449850bdfac380b912267c630e281
alexlee-dev <alex@alexlee.dev>
2020-07-28 07:08:14 -0700

    💅 Styled Components - Convert 'SearchInput' CSS

--- src/client/components/SearchInput/SearchInput.tsx
+++ src/client/components/SearchInput/SearchInput.tsx
@@ -1,11 +1,20 @@
 import * as React from "react";
 import { connect } from "react-redux";
 
-import Button from "./Button";
+import Button from "../Button";
 
-import { searchJobs } from "../redux/thunks";
+import {
+  SearchInputOuterContainer,
+  SearchInputInnerContainer,
+  SearchInputForm,
+  SearchInputLeft,
+  SearchInputInput,
+  SearchInputButtonContainer,
+} from "./SearchInput-styled";
 
-import { LocationOption, RootState } from "../types";
+import { searchJobs } from "../../redux/thunks";
+
+import { LocationOption, RootState } from "../../types";
 
 interface SearchInputProps {
   handleSearch: (search: string, locationOptions: LocationOption[]) => void;
@@ -18,61 +27,37 @@ const SearchInput: React.SFC<SearchInputProps> = (props: SearchInputProps) => {
   const [search, setSearch] = React.useState(searchValue);
 
   return (
-    <div className="search__container__outer">
-      <div className="search__container__inner">
-        <form
+    <SearchInputOuterContainer>
+      <SearchInputInnerContainer>
+        <SearchInputForm
           id="search-form"
           onSubmit={(e) => {
             e.preventDefault();
             handleSearch(search, locationOptions);
           }}
         >
-          <div
-            className="input__addon__left"
-            style={{
-              alignItems: "center",
-              backgroundColor: "#fff",
-              border: "1px solid #b9bdcf",
-              borderBottomLeftRadius: "0.25rem",
-              borderRight: "none",
-              borderTopLeftRadius: "0.25rem",
-              display: "flex",
-              marginRight: "-1px",
-              padding: "0.375rem 0.75rem",
-              paddingRight: "0",
-              textAlign: "center",
-            }}
-          >
-            <i
-              className="material-icons grey icon-sm"
-              style={{
-                color: "#b9bdcf",
-                fontSize: "16px",
-              }}
-            >
-              work_outline
-            </i>
-          </div>
-          <input
+          <SearchInputLeft>
+            <i className="material-icons">work_outline</i>
+          </SearchInputLeft>
+          <SearchInputInput
             aria-label="Title, companies, expertise or benefits"
-            className="search__input"
             id="search"
             onChange={(e) => setSearch(e.target.value)}
             placeholder="Title, companies, expertise or benefits"
             type="text"
             value={search}
           />
-          <div className="search__button__container">
+          <SearchInputButtonContainer>
             <Button
               buttonStyle="primary"
               id="search-submit"
               label="Search"
               type="submit"
             />
-          </div>
-        </form>
-      </div>
-    </div>
+          </SearchInputButtonContainer>
+        </SearchInputForm>
+      </SearchInputInnerContainer>
+    </SearchInputOuterContainer>
   );
 };
 
