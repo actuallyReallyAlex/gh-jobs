e3759abc593adec82e9791e58275d59e08e54403
alexlee-dev <alex@alexlee.dev>
2020-08-03 10:24:32 -0700

    ⚠️ React Error Boundary #53 - Add Sentry

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,10 +1,18 @@
 import * as React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import * as Sentry from "@sentry/react";
+
 import App from "./App";
 import store from "./redux/store";
+
 import "./index.css";
 import "react-toastify/dist/ReactToastify.css";
+
+Sentry.init({
+  dsn:
+    "https://4af7dab133e64ef08765616e040118fe@o202486.ingest.sentry.io/5375872",
+});
 
 ReactDOM.render(
   <Provider store={store}>
