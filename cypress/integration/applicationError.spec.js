a67d53156a93d23645f14b33295aef4b862fcd13
alexlee-dev <alex@alexlee.dev>
2020-08-06 08:43:37 -0700

    ⚠️ ErrorFallback Reset - Home #64 - Add Test Case

--- cypress/integration/applicationError.spec.js
+++ cypress/integration/applicationError.spec.js
@@ -23,16 +23,24 @@ context("Application Error", () => {
         response: {},
         delay: 1000,
       });
+      cy.route({
+        method: "GET",
+        url: "/jobs/f1884b46-ecb4-473c-81f5-08d9bf2ab3bb",
+        status: 200,
+        response: {},
+        delay: 1000,
+      });
     });
     cy.visit("http://localhost:3000");
     cy.wait(500);
+  });
+
+  it("Should display ErrorFallback correctly when an error occurs", () => {
     // * Create an error with stubbed response
     cy.get("#search").type("react");
     cy.get("#search-submit").click();
     cy.wait(1000);
-  });
 
-  it("Should display ErrorFallback correctly when an error occurs", () => {
     cy.get("h1").should("have.text", "Technical Difficulties");
     cy.get("p").should(
       "have.text",
@@ -42,6 +50,20 @@ context("Application Error", () => {
   });
 
   it("Should reset application state when the button is clicked", () => {
+    // * Create an error with stubbed response
+    cy.get("#search").type("react");
+    cy.get("#search-submit").click();
+    cy.wait(1000);
+
+    cy.get("#try-again").click();
+    cy.wait(500);
+    cy.get("#search").should("be.visible");
+  });
+
+  it("Should push app to '/' when 'Try again' button is clicked", () => {
+    cy.get("#f1884b46-ecb4-473c-81f5-08d9bf2ab3bb").click({ force: true });
+    cy.wait(1000);
+
     cy.get("#try-again").click();
     cy.wait(500);
     cy.get("#search").should("be.visible");
