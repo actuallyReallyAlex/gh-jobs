25791be822d363da35559523d6bd86068fa6587e
alexlee-dev <alex@alexlee.dev>
2020-08-10 13:32:51 -0700

    🎨 Redesign Profile Display #47 - Remove old profile sections

--- src/client/pages/Profile/Profile.tsx
+++ src/client/pages/Profile/Profile.tsx
@@ -5,56 +5,22 @@ import { Redirect } from "react-router-dom";
 import {
   ProfileAccountDetails,
   ProfileAccountStats,
-  ProfileDelete,
-  ProfileDisplay,
-  ProfileEdit,
   ProfileHero,
-  ProfileHiddenJobs,
-  ProfileReset,
-  ProfileSavedJobs,
 } from "../../components/Profile";
 
 import {
-  ProfilePage,
-  ProfileForm,
-  ProfileTitleContainer,
   ProfileInnerContainer,
+  ProfilePage,
 } from "../../components/Profile/Profile-styled";
 
 import { RootState } from "../../types";
 
 export interface ProfileProps {
-  isDeletingProfile: boolean;
-  isEditingProfile: boolean;
   isLoggedIn: boolean;
-  isResettingPassword: boolean;
-  isViewingHiddenJobs: boolean;
-  isViewingSavedJobs: boolean;
 }
 
 const Profile: React.SFC<ProfileProps> = (props: ProfileProps) => {
-  const {
-    isDeletingProfile,
-    isEditingProfile,
-    isLoggedIn,
-    isResettingPassword,
-    isViewingHiddenJobs,
-    isViewingSavedJobs,
-  } = props;
-
-  let heading = "Profile";
-
-  if (isResettingPassword) {
-    heading = "Reset Password";
-  } else if (isEditingProfile) {
-    heading = "Edit Profile";
-  } else if (isDeletingProfile) {
-    heading = "Delete Profile";
-  } else if (isViewingHiddenJobs) {
-    heading = "Hidden Jobs";
-  } else if (isViewingSavedJobs) {
-    heading = "Saved Jobs";
-  }
+  const { isLoggedIn } = props;
 
   if (!isLoggedIn) {
     return <Redirect to="/" />;
@@ -66,47 +32,13 @@ const Profile: React.SFC<ProfileProps> = (props: ProfileProps) => {
           <ProfileAccountDetails />
           <ProfileAccountStats />
         </ProfileInnerContainer>
-
-        <ProfileForm
-          isViewingHiddenJobs={isViewingHiddenJobs}
-          isViewingSavedJobs={isViewingSavedJobs}
-          onSubmit={(e) => e.preventDefault()}
-        >
-          <ProfileTitleContainer>
-            <span>
-              <i className="material-icons">account_circle</i>
-            </span>
-            <h1>{heading}</h1>
-          </ProfileTitleContainer>
-
-          {isResettingPassword && <ProfileReset />}
-
-          {isEditingProfile && <ProfileEdit />}
-
-          {isDeletingProfile && <ProfileDelete />}
-
-          {isViewingHiddenJobs && <ProfileHiddenJobs />}
-
-          {isViewingSavedJobs && <ProfileSavedJobs />}
-
-          {!isResettingPassword &&
-            !isEditingProfile &&
-            !isDeletingProfile &&
-            !isViewingHiddenJobs &&
-            !isViewingSavedJobs && <ProfileDisplay />}
-        </ProfileForm>
       </ProfilePage>
     );
   }
 };
 
 const mapStateToProps = (state: RootState) => ({
-  isDeletingProfile: state.user.isDeletingProfile,
-  isEditingProfile: state.user.isEditingProfile,
   isLoggedIn: state.user.isLoggedIn,
-  isResettingPassword: state.user.isResettingPassword,
-  isViewingHiddenJobs: state.user.isViewingHiddenJobs,
-  isViewingSavedJobs: state.user.isViewingSavedJobs,
 });
 
 export default connect(mapStateToProps)(Profile);
