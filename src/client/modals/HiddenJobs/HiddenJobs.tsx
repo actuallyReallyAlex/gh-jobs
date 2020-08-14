c1861823bb3d37a1a3e0b9c9f37929fed366bc77
alexlee-dev <alex@alexlee.dev>
2020-08-14 09:58:17 -0700

    🙈 Hide Job Removes from Current List #75 - Adjust HiddenJobs fetching data

--- src/client/modals/HiddenJobs/HiddenJobs.tsx
+++ src/client/modals/HiddenJobs/HiddenJobs.tsx
@@ -13,7 +13,6 @@ export interface HiddenJobsProps {
   handleGetHiddenJobsDetails: () => void;
   hiddenJobs: string[];
   hiddenJobsDetails: Job[];
-  totalPages: number;
 }
 
 const HiddenJobs: React.SFC<HiddenJobsProps> = (props: HiddenJobsProps) => {
@@ -22,20 +21,27 @@ const HiddenJobs: React.SFC<HiddenJobsProps> = (props: HiddenJobsProps) => {
     handleGetHiddenJobsDetails,
     hiddenJobs,
     hiddenJobsDetails,
-    totalPages,
   } = props;
 
-  const jobsOnPage =
-    hiddenJobsDetails &&
-    hiddenJobsDetails.slice(currentPage * 5 - 5, currentPage * 5);
-
-  const pages = Math.ceil(hiddenJobs.length / 5);
+  const [jobsOnPage, setJobsOnPage] = React.useState([]);
 
   React.useEffect((): void => {
+    if (hiddenJobsDetails) {
+      setJobsOnPage(
+        hiddenJobsDetails.slice(currentPage * 5 - 5, currentPage * 5)
+      );
+    }
+  }, [hiddenJobsDetails]);
+
+  React.useEffect(() => {
     if (hiddenJobs.length > 0) {
       handleGetHiddenJobsDetails();
+    } else {
+      setJobsOnPage([]);
     }
-  }, [totalPages]);
+  }, [hiddenJobs]);
+
+  const pages = Math.ceil(hiddenJobs.length / 5);
 
   return (
     <div>
@@ -53,7 +59,6 @@ const mapStateToProps = (state: RootState) => ({
   currentPage: state.application.currentPage,
   hiddenJobs: state.user.hiddenJobs,
   hiddenJobsDetails: state.user.hiddenJobsDetails,
-  totalPages: state.application.totalPages,
 });
 
 const mapDispatchToProps = (dispatch) => ({
