0378229281c22c869694bb3a12732f837af88ff2
alexlee-dev <alex@alexlee.dev>
2020-08-17 09:07:56 -0700

    🙈 Hide Hidden Jobs in Search by Default #79 - Fix Test for HiddenJobs

--- cypress/integration/hiddenJobs.spec.js
+++ cypress/integration/hiddenJobs.spec.js
@@ -4,20 +4,29 @@ context("Hidden Jobs", () => {
   beforeEach(() => {
     cy.fixture("jobs50").then((jobsJson) => {
       cy.fixture("hiddenDetails").then((hiddenDetailsJson) => {
-        cy.server();
-        cy.route({
-          method: "POST",
-          url: "/jobs",
-          status: 200,
-          response: jobsJson,
-          delay: 1000,
-        });
-        cy.route({
-          method: "GET",
-          url: "/user/hiddenJobsDetails",
-          status: 200,
-          response: hiddenDetailsJson,
-          delay: 1000,
+        cy.fixture("jobDetails").then((jobDetailsJson) => {
+          cy.server();
+          cy.route({
+            method: "POST",
+            url: "/jobs",
+            status: 200,
+            response: jobsJson,
+            delay: 1000,
+          });
+          cy.route({
+            method: "GET",
+            url: "/user/hiddenJobsDetails",
+            status: 200,
+            response: hiddenDetailsJson,
+            delay: 1000,
+          });
+          cy.route({
+            method: "GET",
+            url: "/jobs/f1884b46-ecb4-473c-81f5-08d9bf2ab3bb",
+            status: 200,
+            response: jobDetailsJson,
+            delay: 1000,
+          });
         });
       });
     });
@@ -254,7 +263,7 @@ context("Hidden Jobs - No Results", () => {
     cy.get("#nav-profile").click();
     cy.get("#view-hidden-jobs").click();
 
-    assert.equal(cy.state("requests").length, 3);
+    assert.equal(cy.state("requests").length, 4);
   });
 
   it("Should display correct text", () => {
